9a1d5af5a11573ec01919367a5ee122fdc8629f3
Ayush <ayushverma1016@gmail.com>
2024-12-10 00:02:27 +0530

    update the code

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,12 +3,13 @@ import React from 'react'
 const Navbar = () => {
   return (
     <>
-        <nav className='flex justify-evenly items-center'>
-            <h3 className='text-2xl'>Project1</h3>
+        <nav className='flex justify-evenly items-center py-3 shadow mb-8'>
+            <h3 className='font-bold text-2xl'>Project1</h3>
             <div>
-                <input type="search" name="" id="" placeholder='Search here...' className='outline-none border-2 px-3 py-1' />
+                <input type="search" name="" id="" placeholder='Search here...' className='outline-none border-2 px-3 py-1 rounded-md w-96' />
+                <button className='bg-gray-800 text-white py-1 px-3 ml-2 rounded-md border-2'>Search</button>
             </div>
-            <ul>
+            <ul className='font-semibold flex gap-5'>
                 <li>Home</li>
                 <li>Cart</li>
             </ul>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    getProduct();
+  }, []);
 
-export default Home
+  const getProduct = async () => {
+    const response = await fetch("https://dummyjson.com/products");
+    const data = await response.json();
+    // console.log(data.products);
+
+    setProducts(data.products);
+    console.log(products);
+  };
+
+  return (
+    <>
+      <div className="grid grid-cols-3 justify-center items-center gap-10 w-[70vw] m-auto mb-7 ">
+        {products.map((product,id) => {
+          return (
+            <div key={id} className="w-[250px] bg-gray-300 shadow-xl hover:shadow-blue-500 hover:shadow-lg transition-all ease-linear rounded-md  ml-2 overflow-hidden">
+              <img
+                src={product.images[0]}
+                alt="img"
+                className="bg-gray-200 hover:scale-110 overflow-hidden rounded-md transition-all ease-in duration-500 rounded-bl-none rounded-br-none w-full h-[300px] hover:cursor-grab"
+              />
+              <div className="p-2">
+                <h2 className="font-bold mt-4">{product.title}</h2>
+                <p className="text-xs">
+                  {product.description}
+                </p>
+                <div className="flex justify-between items-center">
+                  <p className="font-semibold py-2">{product.price}</p>
+                  <button className="bg-green-500  px-3 rounded ">Buy</button>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+export default Home;
